f155a4eb16b118705a4f24229f237c4d729014c8
DESKTOP-NLDOG7Q\lol <samfordt@gmail.com>
2018-03-25 16:50:36 -0500

    Added the start menu scene and all of its assets.

---
+++ Stranger to Omelas/Assets/Scripts/FadeInAndOut.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeInAndOut : MonoBehaviour {
+
+    [Tooltip("How fast the test fades in and out.")]
+    public float fadeRate = 1f;
+
+    private Text text;
+    private bool isFading = true;
+
+    //Add refrence to objets text component
+    void Start()
+    {
+        text = gameObject.GetComponent<Text>();
+    }
+
+    void Update ()
+    {
+	    if(isFading)
+        {
+            text.color = new Color(text.color.r, text.color.g, text.color.b, text.color.a - (Time.deltaTime * fadeRate));
+
+            if(text.color.a <=0)
+            {
+                isFading = false;
+            }
+        }
+	}
+}
