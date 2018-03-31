7474225a8029fefd46398ebb4ed702f2ec222532
DESKTOP-NLDOG7Q\lol <samfordt@gmail.com>
2018-03-31 14:14:22 -0500

    Created a LevelManager and SceneTrigger to handle scene transitions and created empty scenes for the remaining levels.

---
+++ Stranger to Omelas/Assets/Scripts/SceneTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneTrigger : MonoBehaviour {
+
+    [Tooltip("Tells trigger which scene to load.")]
+    public string SceneToLoad = "";
+
+    private LevelManager sceneManager;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Load scene " + SceneToLoad);
+    }
+}
