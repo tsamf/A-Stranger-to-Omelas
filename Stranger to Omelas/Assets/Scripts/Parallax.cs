66fd7d13135428db4a0a79c6c959522d2d896d49
DESKTOP-NLDOG7Q\lol <samfordt@gmail.com>
2018-03-31 15:39:01 -0500

    Added the parrallax script to the game and the backgrounds.

---
+++ Stranger to Omelas/Assets/Scripts/Parallax.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour {
+
+    [Tooltip("Sets how fast a parallax is relative to the camera.")]
+    public float parallaxFactor;
+
+    [Tooltip("Sets the direction of the parallax.")]
+    public bool sameDirectionAsCamera = false;
+
+    void Start() {
+        UpdateParrallax();
+    }
+
+    void Update() {
+        UpdateParrallax();
+    }
+
+    void UpdateParrallax()
+    {
+        if (sameDirectionAsCamera)
+        {
+            float newPositionX = Camera.main.transform.position.x * parallaxFactor;
+            transform.position = new Vector3(newPositionX, transform.position.y, transform.position.z);
+        }
+        else
+        {
+            float newPositionX = Camera.main.transform.position.x * parallaxFactor * -1;
+            transform.position = new Vector3(newPositionX, transform.position.y, transform.position.z);
+        }
+    }
+}
