bdb4650c8a5c254a45e71d41c022568d1c72321d
DESKTOP-NLDOG7Q\lol <samfordt@gmail.com>
2018-03-31 13:49:56 -0500

    Added a basic character controller for testing and a camera follow script.

---
+++ Stranger to Omelas/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    GameObject stranger;
+    public float minimumCameraX = 0.0f;
+    public float maximumCameraX = 0.0f;
+
+    void Awake()
+    {
+        stranger = GameObject.FindGameObjectWithTag("Stranger");
+        float newXPostion = Mathf.Clamp(stranger.transform.position.x, minimumCameraX, maximumCameraX);
+        transform.position = new Vector3(newXPostion, transform.position.y, transform.position.z);
+    }
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float newXPostion = Mathf.Clamp(stranger.transform.position.x, minimumCameraX, maximumCameraX);
+        transform.position = new Vector3(newXPostion, transform.position.y, transform.position.z);
+    }
+}
---
+++ Stranger to Omelas/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    [Tooltip("How fast the player moves.")]
+    public float speed = 10.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+   	void Update () {
+	    if(Input.GetKey(KeyCode.A))
+        {
+            transform.position -= new Vector3(speed * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += new Vector3(0, speed * Time.deltaTime, 0);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position -= new Vector3(0, speed * Time.deltaTime, 0);
+        }
+
+    }
+}
